ccb05d1b444525e982043da8fce225a8db6e2bdc
Documenter.jl <documenter@juliadocs.github.io>
2022-10-19 12:45:05 +0000

    build based on 3a12013

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = myOmniscape","category":"page"},{"location":"#myOmniscape","page":"Home","title":"myOmniscape","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for myOmniscape.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [myOmniscape]","category":"page"}]
+}
